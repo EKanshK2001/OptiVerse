0044b9ab0ff7d61371c5f34999ef122ef0ba3f64
Ekansh <efuforofficial@gmail.com>
2023-11-14 22:58:42 +0530

    fixed create post area in homepage

--- scripts.js
+++ scripts.js
@@ -1,25 +1,7 @@
+function mySearch() {
+    // document.getElementById('home').classList.remove('active');
 
-function myMenuFunction() {
-    var i = document.getElementById("navMenu");
-
-    if (i.className === "nav-menu") {
-        i.className += " responsive";
-    } else {
-        i.className = "nav-menu";
-    }
+    var searchBox = document.getElementById('search');
+    
+    searchBox.style.visibility = 'hidden';
 }
-
-
-// var a = document.getElementById("loginBtn");
-// var b = document.getElementById("registerBtn");
-
-function login() {
-    location.href = "loginpage.html";
-};
-
-function register() {
-    location.href = "registerpage.html"
-};
-
-
-
